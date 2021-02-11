f506db18ba6371dfe86a6a05756e672406904ebc
Wesley <wspencer3295@yahoo.com>
2021-02-11 15:50:18 -0700

    commit tree a106072f4d4af46d7dd2f5500caac2380ab556ca

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards( namesArray, event) {
+    let thankYouCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(count) {
+    while(count > 0){
+    console.log(count)
+    count -= 1;
+    }
+    console.log(count)
+}
